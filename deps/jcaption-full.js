eed6889f1a9b5889ad4de5f49f6fd9ff82bb4d40
Mikhail B <wproxym@at.gmail>
2023-04-15 11:16:27 +0300

    hide jcaption undefined

---
+++ deps/jcaption-full.js
@@ -0,0 +1,40 @@
+/**
+ * JCaption javascript behavior
+ *
+ * Used for displaying image captions
+ *
+ * @package  Joomla
+ * @since    1.5
+ * @version  1.1
+ */
+
+jQuery.fn.JCaption = function () {
+	'use strict';
+
+	var $ = jQuery;
+	var selector = this.selector.replace('.', '_');
+
+    return this.each(function () {
+        var $el = $(this);
+        var caption = $el.prop('title');
+        var width = $el.prop('width') || this.width;
+        var align = $el.prop('align') || $el.css('float') || this.style.float || 'none';
+
+        var $container = $('<div/>', {
+            'class' : selector + ' ' + align,
+            'css' : {
+                'float' : align,
+                'width' : width
+            }
+        }).insertBefore($el).append($el);
+
+        if (caption !== '') {
+            $('<p/>', {
+                'text' : caption,
+                'class' : selector
+            }).appendTo($container);
+        }
+    });
+};
+
+function JCaption (selector) { jQuery(selector).JCaption(); }
